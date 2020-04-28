a285afc36d16011806a44abb10d9deb557d4f5d6
yashdatir <yashdatir@gmail.com>
2020-04-29 01:33:28 +0530

    First commit

--- App.tsx
+++ App.tsx
@@ -1,16 +1,19 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, Alert } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={{fontWeight:'900'}}>Welcome Yash Datir</Text>
+      <Text style={{color:'red'}}>This is Android | IOS</Text>
+      <Button title="Click" onPress={()=>Alert.alert('Hello World')} />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    marginTop:'40%',
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
